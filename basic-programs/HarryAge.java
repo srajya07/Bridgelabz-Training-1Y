a3387ffab324b369a82be033e634139e8dc062a6
srajya07 <srajya066gmail.com>
2026-02-09 20:14:34 +0530

    [Rajya Vardhan]Add.Harry's Age

---
+++ basic-programs/HarryAge.java
@@ -0,0 +1,10 @@
+class HarryAge {
+    public static void main(String[] args) {
+        int B.Year = 2000;
+        int C.Year = 2024;
+
+        int age = C.Year - B.Year;
+
+        System.out.println("Harry's age in 2024 is " + age);
+    }
+}
