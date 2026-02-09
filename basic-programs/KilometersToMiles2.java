43d256b53fe563cd20c4f017f50e939832fbd16e
srajya07 <srajya066gmail.com>
2026-02-09 20:25:19 +0530

    [Rajya Vardhan]Add.10.8 Kilometers to miles

---
+++ basic-programs/KilometersToMiles2.java
@@ -0,0 +1,14 @@
+class KilometerToMiles2 {
+public static void main (String[] args) {
+
+   double Kilometer = 10.8;
+   System.out.println("Distance in Kilometer :" + Kilometer);
+   
+   double miles = Kilometer*0.62;
+   
+   System.out.println("The Distance" + Kilometer + "km in miles is :" + miles);
+   
+  }
+}
+   
+   
