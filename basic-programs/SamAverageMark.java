5b54989b371d5933215b5e8d4bfd17dda81e9201
srajya07 <srajya066gmail.com>
2026-02-09 20:19:50 +0530

    [Rajya Vardhan]Add.Sam's Average Marks in PCM

---
+++ basic-programs/SamAverageMark.java
@@ -0,0 +1,14 @@
+class SamAverageMark {
+    public static void main(string[] args) {
+        System.out.println("Sam's Marks in Maths : 94");
+        int maths = 94;
+        System.out.println("Sam's Marks in Chemistry : 96");
+		int chemistry =96;
+        System.out.println("Sam's Marks in Physics : 95");
+		int physics = 95;
+
+        double avr = (maths+chemistry+physics)/3.0;
+		
+		System.out.println("Sam's Average Marks in PCM :" + avr);
+	   }
+	}
