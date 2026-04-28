a3e3d78e5d9e317d4bfcf121711eb563dbb600b6
srajya07 <srajya066gmail.com>
2026-04-29 00:03:25 +0530

    [Rajya Vardhan]Add.Reverse Array Programe

---
+++ src/com/gla/arrays/Reverse.java
@@ -0,0 +1,30 @@
+package com.gla.arrays;
+import java.util.Scanner;
+
+public class Reverse {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt();
+        if (number == 0) {
+            System.out.println("Reversed number: 0");
+            return;
+        }
+        int temp = number;
+        int count = 0;
+        while (temp != 0) {
+            count++;
+            temp /= 10;
+        }
+        int[] digits = new int[count];
+        temp = number;
+        for (int i = 0; i < count; i++) {
+            digits[i] = temp % 10;
+            temp /= 10;
+        }
+        System.out.print("Reversed number: ");
+        for (int i = 0; i < count; i++) {
+            System.out.print(digits[i]);
+        }
+    }
+}
