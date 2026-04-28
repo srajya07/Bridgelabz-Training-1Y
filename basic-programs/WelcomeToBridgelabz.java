915cafbc3ef9416357e7e44fbaf879ef828a7d4e
srajya07 <srajya066gmail.com>
2026-04-28 22:51:12 +0530

    [Rajya Vardhan]Add.Basic Programe

---
+++ basic-programs/WelcomeToBridgelabz.java
@@ -0,0 +1,10 @@
+public class WelcomeToBridgelabz {
+    public static void m1() {
+      System.out.println("Welcome to bridgelabz!");
+   }
+
+   public static void main(String[] var0) {
+      m1();
+   }
+    
+}
